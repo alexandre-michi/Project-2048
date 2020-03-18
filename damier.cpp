5de6156ebef6330b9230def6c346d50802e3b425
Smeelock <aurelien.vungoc@gmail.com>
2020-03-18 15:38:16 +0100

    (movement working) 2048 in console ready

--- damier.cpp
+++ damier.cpp
@@ -3,10 +3,13 @@
 #include <iostream>
 #include <ctime> // to "unseed" rand function with srand and ctime
 
+using namespace std;
+
+
 // Constructeur : allocation de mémoire et initialisation à zéro
 Damier::Damier(int size)
 {
-    int Size = size;
+    Size = size;
     tab = new int*[Size];
 
     for(int i=0; i<Size; i++){
@@ -18,26 +21,33 @@ Damier::Damier(int size)
     randomTile();
 }
 
+// Destructor
+Damier::~Damier()
+{
+    std::cout << "Destruction !" << std::endl;
+}
+
 // Place un 2 ou un 4 au hasard sur le damier
 void Damier::randomTile()
 {
+    srand(time(NULL));
     int i = rand() % Size;
     int j = rand() % Size;
 
     // On s'assure que la case est vide, sinon on en cherche une autre
     while(tab[i][j] != 0){
-        int i = rand() % Size;
-        int j = rand() % Size;
+        i = rand() % Size;
+        j = rand() % Size;
     }
 
     tab[i][j] = 2; // Possibilité de changer avec 4
 }
 
-bool Damier::checkDefeat()
+bool Damier::isDefeat()
 {
-    for (size_t i = 0; i < Size; i++)
+    for (int i = 0; i < Size; i++)
     {
-        for (size_t j = 0; j < Size; j++)
+        for (int j = 0; j < Size; j++)
         {
             int t = tab[i][j]; 
             if (t == 0){ return false;} // there is an empty tile
@@ -51,19 +61,19 @@ bool Damier::checkDefeat()
     return true;   
 }
 
-std::queue<int> Damier::clearArray(int t[], int mvt)
+std::queue<int> Damier::clearArray(const int t[], int mvt)
 {
     std::queue<int> my_queue; // lifo ie un paquet de gâteaux
-    if (mvt == UP || mvt == LEFT)
+    if (mvt == UP || mvt == LEFT) // remplissage par la gauche
     {
-        for (size_t i = 0; i < Size; i++)
+        for (int i = 0; i < Size; i++)
         {
             if (t[i] != 0){ my_queue.push(t[i]);}
         }
     }
     else
-    {
-        for (size_t i = Size-1; i >= 0; i--)
+    {        
+        for (int i = Size-1; i >= 0; i--) // remplissage par la droite
         {
             if (t[i] != 0){ my_queue.push(t[i]);}
         }
@@ -73,13 +83,14 @@ std::queue<int> Damier::clearArray(int t[], int mvt)
 
 void Damier::process(int movement)
 {
+    cout << "Process avec movement = " << movement << endl;
     if (movement == UP) 
     {
-        for (size_t j = 0; j < Size; j++)
+        for (int j = 0; j < Size; j++)
         {
-            size_t index = 0; // to fill tab
+            int index = 0; // to fill tab
             int *col = new int[Size];
-            for (size_t i = 0; i < Size; i++)
+            for (int i = 0; i < Size; i++)
             {
                 col[i] = tab[i][j];
                 tab[i][j] = 0; // reset column by setting everything to 0
@@ -97,16 +108,21 @@ void Damier::process(int movement)
                     tab[index][j] = curr_num * 2; // fill tab
                     index++;                      // increase index
                 }
+                else // no fustion needed
+                {
+                    tab[index][j] = curr_num; // fill tab with curr_num (not fusionned)
+                    index++;                  // increase index
+                }
             }
         }
     }
     else if (movement == DOWN)
     {
-        for (size_t j = 0; j < Size; j++)
+        for (int j = 0; j < Size; j++)
         {
-            size_t index = Size-1; // to fill tab
+            int index = Size-1; // to fill tab
             int *col = new int[Size];
-            for (size_t i = 0; i < Size; i++)
+            for (int i = 0; i < Size; i++)
             {
                 col[i] = tab[i][j];
                 tab[i][j] = 0; // reset column by setting everything to 0
@@ -124,17 +140,23 @@ void Damier::process(int movement)
                     tab[index][j] = curr_num * 2; // fill tab
                     index--;                      // increase index
                 }
+                else // no fustion needed
+                {
+                    tab[index][j] = curr_num; // fill tab with curr_num (not fusionned)
+                    index--;                  // increase index
+                }
             }
         }
     }
     else if (movement == LEFT)
     {
-        for (size_t i = 0; i < Size; i++)
+        for (int i = 0; i < Size; i++)
         {
-            size_t index = 0; // to fill tab
+            int index = 0; // to fill tab
             int *line = tab[i];
             std::queue<int> my_queue = clearArray(line, movement); // queue with no 0
-            for (size_t j = 0; j < Size; j++)
+            
+            for (int j = 0; j < Size; j++)
             {
                 tab[i][j] = 0; // reset line by setting everything to 0
             } 
@@ -149,17 +171,22 @@ void Damier::process(int movement)
                     tab[i][index] = curr_num * 2; // fill tab
                     index++;                      // increase index
                 }
+                else // no fustion needed 
+                {
+                    tab[i][index] = curr_num;    // fill tab with curr_num (not fusionned)
+                    index++;                      // increase index
+                }
             }
         }
     }
     else if (movement == RIGHT)
     {
-        for (size_t i = 0; i < Size; i++)
+        for (int i = 0; i < Size; i++)
         {
-            size_t index = Size-1; // to fill tab
+            int index = Size-1; // to fill tab
             int *line = tab[i];
             std::queue<int> my_queue = clearArray(line, movement); // queue with no 0
-            for (size_t j = 0; j < Size; j++)
+            for (int j = 0; j < Size; j++)
             {
                 tab[i][j] = 0; // reset line by setting everything to 0
             } 
@@ -174,18 +201,25 @@ void Damier::process(int movement)
                     tab[i][index] = curr_num * 2; // fill tab
                     index--;                      // decrease index
                 }
+                else // no fustion needed
+                {
+                    tab[i][index] = curr_num; // fill tab with curr_num (not fusionned)
+                    index--;                  // increase index
+                }
             }
         }
     }
+
+    // TODO : create error if not the right movement
 
     randomTile();
 }
 
 void Damier::consolePrint()
 {
-    for (size_t i = 0; i < Size; i++)
+    for (int i = 0; i < Size; i++)
     {
-        for (size_t j = 0; j < Size; j++)
+        for (int j = 0; j < Size; j++)
         {
             std::cout << tab[i][j] << ' ';
         }

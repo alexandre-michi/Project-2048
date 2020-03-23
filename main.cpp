2a2a49ef2a9e13734474e747cc2d37fb2811e524
Smeelock <aurelien.vungoc@gmail.com>
2020-03-23 16:40:20 +0100

    adding includes and code to execute main window

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
-#include "mainwindow.h"
+// #include "mainwindow.h"
+
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <QtQml>
 
 #include "damier.h"
 
-#include <QApplication>
+//#include <QApplication>
 #include <iostream>
 
 using namespace std;
@@ -16,13 +20,22 @@ int main(int argc, char *argv[])
     {
         d.consolePrint();
         
-        cout << "   0 : UP" << endl;
-        cout << "   1 : RIGHT" << endl;
-        cout << "   2 : DOWN" << endl;
-        cout << "   3 : LEFT" << endl;
-        cout << "Movement : ";
-        cin >> movement;
-        d.process(movement);
+
+    // QT quick application
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+    const QUrl url(QStringLiteral("qrc:/main.qml"));
+    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
+                     &app, [url](QObject *obj, const QUrl &objUrl) {
+        if (!obj && url == objUrl)
+            QCoreApplication::exit(-1);
+    }, Qt::QueuedConnection);
+    engine.load(url);
+
+    return app.exec();
     }
     
     // QApplication a(argc, argv);

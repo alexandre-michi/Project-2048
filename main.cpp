cfa0c8a5da470f5234f0a4e9b7559a072e092c53
Smeelock <aurelien.vungoc@gmail.com>
2020-03-23 16:41:05 +0100

    test

--- main.cpp
+++ main.cpp
@@ -16,10 +16,19 @@ int main(int argc, char *argv[])
     Damier d = Damier(4);
     int movement;
 
-    while (!d.isDefeat())
-    {
-        d.consolePrint();
-        
+//    while (!d.isDefeat())
+//    {
+//        d.consolePrint();
+
+//        cout << "   0 : UP" << endl;
+//        cout << "   1 : RIGHT" << endl;
+//        cout << "   2 : DOWN" << endl;
+//        cout << "   3 : LEFT" << endl;
+//        cout << "Movement : ";
+//        cin >> movement;
+//        d.process(movement);
+//    }
+
 
     // QT quick application
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
@@ -36,11 +45,30 @@ int main(int argc, char *argv[])
     engine.load(url);
 
     return app.exec();
-    }
-    
-    // QApplication a(argc, argv);
-    // MainWindow w;
-    // w.show();
-    // return a.exec();
-    return 0;
 }
+
+
+//int main(int argc, char *argv[])
+//{
+//    Damier d = Damier(4);
+//    int movement;
+
+//    while (!d.isDefeat())
+//    {
+//        d.consolePrint();
+        
+//        cout << "   0 : UP" << endl;
+//        cout << "   1 : RIGHT" << endl;
+//        cout << "   2 : DOWN" << endl;
+//        cout << "   3 : LEFT" << endl;
+//        cout << "Movement : ";
+//        cin >> movement;
+//        d.process(movement);
+//    }
+    
+//    // QApplication a(argc, argv);
+//    // MainWindow w;
+//    // w.show();
+//    // return a.exec();
+//    return 0;
+//}
